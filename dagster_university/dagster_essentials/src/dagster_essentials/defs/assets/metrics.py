20af5c887680c4a8749411369c6e13e2d1703938
Marina Tcai <marina.tcai@offis.de>
2026-01-26 21:23:11 +0100

    added viz for manchattan stat asset

--- dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
@@ -49,3 +49,22 @@ def manhattan_stats() -> None:
     # Opens (or creates) a file at the path specified in constants.MANHATTAN_STATS_FILE_PATH and writes the trip statistics as JSON to that file.
     with open(constants.MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
         output_file.write(trips_by_zone.to_json())
+
+
+# src/dagster_essentials/defs/assets/metrics.py
+@dg.asset(
+    deps=["manhattan_stats"],
+)
+def manhattan_map() -> None:
+    trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    trips_by_zone.plot(column="num_trips", cmap="plasma", legend=True, ax=ax, edgecolor="black")
+    ax.set_title("Number of Trips per Taxi Zone in Manhattan")
+
+    ax.set_xlim(-74.05, -73.90)  # Adjust longitude range
+    ax.set_ylim(40.70, 40.82)  # Adjust latitude range
+    
+    # Save the image
+    plt.savefig(constants.MANHATTAN_MAP_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
